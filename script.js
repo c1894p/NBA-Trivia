990cb8007a4132c019e4090c9f1346711ea85700
c1894p <csp1894@gmail.com>
2021-04-08 13:32:54 -0400

    new style sheet and restart btn added

--- script.js
+++ script.js
@@ -6,23 +6,48 @@ const questions = [
   },
   {
     question: "Which NBA franchise has won the most NBA titles?",
-    choices: [
-      "Boston Celtics",
-      "New York Knicks",
-      "Los Angelas Lakers",
-      "Chicago Bulls",
-    ],
+    choices: ["Boston Celtics","New York Knicks","Los Angelas Lakers","Chicago Bulls",],
     answer: "Los Angelas Lakers",
   },
   {
     question: "Who has scored the most points ever in a NBA game?",
-    choices: [
-      "Kobe Bryant",
-      "Michael Jordan",
-      "Devin Booker",
-      "Wilt Chamberlin",
-    ],
-    answer: "Los Angelas Lakers",
+    choices: ["Kobe Bryant","Michael Jordan","Devin Booker","Wilt Chamberlin"],
+    answer: "Wilt Chamberlin",
+  },
+  {
+    question: "Who is the first player to drafted #1 with out playing college or highschool basketball in the NBA?",
+    choices: ["Luka Doncic","Yoa Ming","Dikembe Mutombo","Giannis Antetokounmpo"],
+    answer: "Yao Ming",
+  },
+  {
+    question: "Which team drafted Kobe Bryant in 1996?",
+    choices: ["Charlotte Hornets","Los Angelas Lakers","Chicago Bulls","New York Knicks"],
+    answer: "Charlotte Hornets",
+  },
+  {
+    question: "Which team has won the most consecutive NBA titles?",
+    choices: ["Detroit Pistons","Boston Celtics","Los Angelas Lakers","Chicago Bulls"],
+    answer: "Boston Celtics",
+  },
+  {
+    question: "Which player has won the most NBA dunk contests?",
+    choices: ["Dominique Wilkins","Aaron Gordon","Michael Jordan","Nate Robinson"],
+    answer: "Nate Robinson",
+  },
+  {
+    question: "Which player has the record for most 3-pointers in a game?",
+    choices: ["Steph Curry","Ray Allen","Klay Thompson","Reggie Miller"],
+    answer: "Klay Thompson",
+  },
+  {
+    question: "Who was the youngest player to score 10,000 points in the NBA?",
+    choices: ["Kobe Bryant","Lebron James","Michael Jordan","Wilt Chamberlin"],
+    answer: "Lebron James",
+  },
+  {
+    question: "Which team owns the record for the longest winning streak in NBA history?",
+    choices: ["Chicago Bulls","Golden State Warriors","Los Angelas Lakers","Miami Heat"],
+    answer: "Golden State Warriors",
   },
 ];
 
@@ -32,20 +57,23 @@ const choiceA = document.querySelector(".A");
 const choiceB = document.querySelector(".B");
 const choiceC = document.querySelector(".C");
 const choiceD = document.querySelector(".D");
+const choiceBtns = document.querySelectorAll(".choice");
 const nextQBtn = document.querySelector("#nextQ");
 
 //function to set questions
-let q = 0
-function setQuestion2() {
-    h2.innerText = questions[q].question;
-    choiceA.innerText = questions[q].choices[0];
-    choiceB.innerText = questions[q].choices[1];
-    choiceC.innerText = questions[q].choices[2];
-    choiceD.innerText = questions[q].choices[3];
+let q = 0;
+function setQuestion() {
+  h2.innerText = questions[q].question;
+  choiceA.innerText = questions[q].choices[0];
+  choiceB.innerText = questions[q].choices[1];
+  choiceC.innerText = questions[q].choices[2];
+  choiceD.innerText = questions[q].choices[3];
 
     q++;
 }
 
 //event listner
-window.addEventListener("load", setQuestion2);
-nextQBtn.addEventListener("click", setQuestion2);
+window.addEventListener("load", setQuestion);
+nextQBtn.addEventListener("click", setQuestion);
+
+
