79c2590de84a84c0bda5cd0ce0e53deb7d2c921f
c1894p <csp1894@gmail.com>
2021-04-09 15:52:22 -0400

    homepage rules intro song

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-const ruleBtn = document.querySelector('#ruleBtn');
-const ruleDiv = document.querySelector('#rules');
-const closeBtn = document.querySelector('#closeBtn');
+const ruleBtn = document.querySelector("#ruleBtn");
+const ruleDiv = document.querySelector("#rules");
+const closeBtn = document.querySelector("#closeBtn");
+const bulls = document.querySelector("#bulls");
 
-ruleBtn.addEventListener('click', () => {
-    ruleDiv.style.display ="block";
-    ruleBtn.style.display ="none"
-})
+ruleBtn.addEventListener("click", () => {
+  ruleDiv.style.display = "block";
+  ruleBtn.style.display = "none";
+  bulls.play();
+});
 
-closeBtn.addEventListener('click', () => {
-    ruleDiv.style.display ="none";
-    ruleBtn.style.display ="block"
-})
+closeBtn.addEventListener("click", () => {
+  ruleDiv.style.display = "none";
+  ruleBtn.style.display = "block";
+});
+
